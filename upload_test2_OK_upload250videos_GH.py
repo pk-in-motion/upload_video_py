6f096839e871e8abbd80b7ee74e634a784089ecf
pk-in-motion <purnadi.k@gmail.com>
2022-07-17 21:43:54 +0800

    update code for uploading 250 videos

--- upload_test2_OK_upload250videos_GH.py
+++ upload_test2_OK_upload250videos_GH.py
@@ -9,12 +9,24 @@ d.set_grant_type('password', api_key="...", api_secret="..",
     scope=['manage_videos'], info={'username': "...", 'password': "..."})
 url = d.upload('./xmas3_short.mp4')
 
+
+'''
+# upload one video = number 000
 c = 000
-#while c < 250:
 up = d.post('/me/videos',
         {'url': url, 'title': 'Video '+ str(c), 'published': 'true', 'channel': 'tech','is_created_for_kids': 'false'})
 print (up['id'])
-#    c = c + 1
+'''
+
+
+
+# upload 250 video
+while c < 250:
+up = d.post('/me/videos',
+        {'url': url, 'title': 'Video '+ str(c), 'published': 'true', 'channel': 'tech','is_created_for_kids': 'false'})
+print (up['id'])
+    c = c + 1
+
 
 
 
