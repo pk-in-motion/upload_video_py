bc66a5def28c2a01307cbe57e46fa8da8a4ff4fd
pk-in-motion <purnadi.k@gmail.com>
2022-07-17 21:38:41 +0800

    upload 250 videos

---
+++ upload_test2_OK_upload250videos_GH.py
@@ -0,0 +1,22 @@
+import dailymotion
+
+from requests_toolbelt import MultipartEncoder
+import requests
+
+
+d = dailymotion.Dailymotion()
+d.set_grant_type('password', api_key="...", api_secret="..",
+    scope=['manage_videos'], info={'username': "...", 'password': "..."})
+url = d.upload('./xmas3_short.mp4')
+
+c = 000
+#while c < 250:
+up = d.post('/me/videos',
+        {'url': url, 'title': 'Video '+ str(c), 'published': 'true', 'channel': 'tech','is_created_for_kids': 'false'})
+print (up['id'])
+#    c = c + 1
+
+
+
+#video_id = (up['private_id'])
+#print ("is done")
